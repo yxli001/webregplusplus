2c9aad04ba8bc69d2e8e4942eb4e1f0d4528de13
yxli001 <lxd520anna@gmail.com>
2025-03-09 21:59:15 -0700

    add error messages to zod validateEnv

--- backend/src/util/validateEnv.ts
+++ backend/src/util/validateEnv.ts
@@ -1,11 +1,21 @@
 import z from "zod";
 
 const envSchema = z.object({
-  POSTGRES_HOST: z.string(),
-  POSTGRES_PORT: z.string(),
-  POSTGRES_USER: z.string(),
-  POSTGRES_PASSWORD: z.string(),
-  POSTGRES_DB: z.string(),
+  POSTGRES_HOST: z.string({
+    message: "Missing POSTGRES_HOST in environment variables",
+  }),
+  POSTGRES_PORT: z.string({
+    message: "Missing POSTGRES_PORT in environment variables",
+  }),
+  POSTGRES_USER: z.string({
+    message: "Missing POSTGRES_USER in environment variables",
+  }),
+  POSTGRES_PASSWORD: z.string({
+    message: "Missing POSTGRES_PASSWORD in environment variables",
+  }),
+  POSTGRES_DB: z.string({
+    message: "Missing POSTGRES_DB in environment variables",
+  }),
 });
 
 export default envSchema.parse(process.env);
