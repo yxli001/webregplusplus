e4b535ceb33570e8ebadeb5ab4ce22c254162029
yxli001 <lxd520anna@gmail.com>
2025-03-29 18:11:46 -0700

    combine server logs and api logs

--- backend/src/middleware/logger.ts
+++ backend/src/middleware/logger.ts
@@ -1,31 +1,8 @@
+import logger from "@/util/logger";
 import { RequestHandler } from "express";
-import fs from "fs";
-import path from "path";
 
-const fsPromises = fs.promises;
+export const log: RequestHandler = (req, res, next) => {
+  logger.info(`${req.method}\t${req.url}\t${req.headers.origin}`);
 
-export const logEvents = async (message: string, logFileName: string) => {
-  console.log(message);
-
-  const dateTime = new Date();
-  const logItem = `${dateTime}\t${message}\n`;
-
-  try {
-    // Create logs directory if it doesn't exist
-    if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
-      await fsPromises.mkdir(path.join(__dirname, "..", "logs"));
-    }
-
-    await fsPromises.appendFile(
-      path.join(__dirname, "..", "logs", logFileName),
-      logItem,
-    );
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-export const logger: RequestHandler = (req, res, next) => {
-  logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, "reqLog.log");
   next();
 };
