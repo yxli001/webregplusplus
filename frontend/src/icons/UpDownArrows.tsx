7a224814b2145d0e731d236081eb78e720248e8a
yxli001 <lxd520anna@gmail.com>
2025-03-30 22:13:13 -0700

    update arrows icon

--- frontend/src/icons/UpDownArrows.tsx
+++ frontend/src/icons/UpDownArrows.tsx
@@ -3,8 +3,8 @@ import { SVGProps } from "react";
 const UpDownArrows = ({ color, ...props }: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
+    width="10"
+    height="16"
     fill="none"
     {...props}
   >
