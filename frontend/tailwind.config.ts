24cfc01b5d46ed69ef3dc0f55a7d062c952bf234
yxli001 <lxd520anna@gmail.com>
2025-03-30 21:42:18 -0700

    add border color

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -11,6 +11,7 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        border: "var(--border)",
         primary: {
           light: "var(--primary)",
           dark: "var(--primary-dark)",
