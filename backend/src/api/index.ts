df6e5b22524ec5e3d2cfa0fee10fdd1b1d45bfa8
yxli001 <lxd520anna@gmail.com>
2025-05-18 13:03:42 -0700

    manually add aliases for serverless function

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,4 +1,10 @@
-import "module-alias/register";
+import { addAliases } from "module-alias";
+import { join } from "path";
+
+// manually add aliases for serverless function
+addAliases({
+  "@": join(__dirname, ".."),
+});
 
 import serverless from "serverless-http";
 import app from "@/app";
