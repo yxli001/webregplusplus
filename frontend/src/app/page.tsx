873a52bddfd2a9a896659118e2d2153acfda1e55
yxli001 <lxd520anna@gmail.com>
2025-07-06 16:31:07 -0700

    remove testing stuff from homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,37 +1,3 @@
-import ButtonGroup from "@/components/ButtonGroup";
-import Book from "@/icons/Book";
-import Preferences from "@/icons/Preferences";
-
 export default function Home() {
-  return (
-    <div>
-      <ButtonGroup
-        buttons={[
-          {
-            label: "Calendar",
-            active: true,
-          },
-          {
-            label: "Finals",
-          },
-          {
-            label: "List",
-          },
-        ]}
-      />
-      <ButtonGroup
-        buttons={[
-          {
-            label: "Added",
-            active: true,
-            icon: Book,
-          },
-          {
-            label: "Preferences",
-            icon: Preferences,
-          },
-        ]}
-      />
-    </div>
-  );
+  return <div>Home</div>;
 }
