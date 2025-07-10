1dbdbaeb98a85c20e9f02386990c4f1f3bb7db13
Yixuan Li <54194100+yxli001@users.noreply.github.com>
2025-07-10 11:28:02 -0700

    Update Checkbox component to support intermediate state (#67)
    * update Checkbox component to support intermediate state
    
    * improve accessibility on Checkbox

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,45 @@
+"use client";
+
+import { useState } from "react";
+
+import { getCourses } from "@/api/courses";
+import Checkbox from "@/components/Checkbox";
+import CourseDropdown from "@/components/CourseDropdown";
+import { usePreferenceStore } from "@/hooks/usePreferenceStore";
+
 export default function Home() {
-  return <div>Home</div>;
+  const selectedQuarter = usePreferenceStore((state) => state.selectedQuarter);
+  const [checked, setChecked] = useState<undefined | boolean>(undefined);
+  const [loadingCourses, setLoadingCourses] = useState(false);
+
+  const fetchCourses = async (query: string) => {
+    if (!selectedQuarter) {
+      return [];
+    }
+
+    setLoadingCourses(true);
+
+    const courses = await getCourses(selectedQuarter, query);
+
+    setLoadingCourses(false);
+
+    if (!courses.success) {
+      return [];
+    }
+
+    return courses.data;
+  };
+
+  return (
+    <div>
+      {selectedQuarter && (
+        <CourseDropdown
+          className="w-1/2"
+          fetchCourses={fetchCourses}
+          loading={loadingCourses || !selectedQuarter}
+        />
+      )}
+      <Checkbox checked={checked} onChange={setChecked} />
+    </div>
+  );
 }
